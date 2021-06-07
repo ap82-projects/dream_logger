413b2f713d5ed9638b724398ec4228d058b2e9b9
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-06-07 17:35:32 +0900

    Established database connection.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
-// import * as express from "express";
-// import * as cors from "cors";
 const express = require("express");
 const cors = require("cors");
+const knex = require("./knex");
+const { createConnection } = require("typeorm");
+const dbConfig = require("./ormconfig");
+// import { createConnection } from "typeorm";
+// import dbConfig from "./ormconfig"
+
 const app = express();
-const port = process.env.DB_PORT || 9999
+const port = process.env.SERVER_PORT || 9999
 
 app.use(express.json());
 app.use(cors());
@@ -13,4 +17,14 @@ app.listen(port, () => {
 
 app.get("/api/test_backend", async (req, res) => {
   res.send("Connected To Backend");
+});
+
+app.get("/api/test_database", async (req, res) => {
+  try {
+    await createConnection(dbConfig)
+    res.send("Connected to database.");
+  } catch(err) {
+    console.log(err);
+    res.send("Cannot connect to database.");
+  }
 });
